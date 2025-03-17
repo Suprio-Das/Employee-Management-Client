27fb37a6d4085ce6771360b2dd940448e2fcc225
Suprio-Das <priyamd507@gmail.com>
2025-03-17 21:06:18 +0600

    Employees and Add Employees component created(Design not completed)

---
+++ src/Components/AddEmployees.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddEmployees = () => {
+    return (
+        <div>
+            <h1>This is Add Employees.</h1>
+        </div>
+    );
+};
+
+export default AddEmployees;
---
+++ src/Components/Employees.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Employees = () => {
+    return (
+        <div>
+            <h1>This is Employees</h1>
+        </div>
+    );
+};
+
+export default Employees;
