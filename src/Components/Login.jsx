687bcb9a5ab45510c2b11ba24275fea00f7e03ea
Suprio-Das <priyamd507@gmail.com>
2025-03-17 23:35:43 +0600

    Login Page logic modified according to the session

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { AuthContext } from '../Provider/AuthProvider';
 
 const Login = () => {
-    const { loading, signIn } = useContext(AuthContext);
+    const { signIn, user } = useContext(AuthContext);
     const handleLogin = e => {
         e.preventDefault();
         const form = e.target;
@@ -21,20 +21,29 @@ const Login = () => {
         <div>
             <div className="bg-base-200 min-h-[calc(100vh-65px)] flex flex-col justify-center items-center">
                 <div className="text-center">
-                    <h1 className="text-5xl font-bold my-5">Login to the Systema!</h1>
+                    {
+                        user ?
+                            <h1 className="text-5xl font-bold my-5">You're into the Systema! <br /> Try to do something C<span className='text-6xl text-orange-600'>O-O</span>L.</h1>
+                            :
+                            <h1 className="text-5xl font-bold my-5">Login to the Systema!</h1>
+                    }
                 </div>
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <div className="card-body">
-                        <form onSubmit={handleLogin} className="fieldset">
-                            <label className="fieldset-label">Email</label>
-                            <input type="email" className="input" name='email' placeholder="Email" />
-                            <label className="fieldset-label">Password</label>
-                            <input type="password" className="input" name='password' placeholder="Password" />
-                            <div><a className="link link-hover">Forgot password?</a></div>
-                            <button type='submit' className="btn btn-neutral mt-4">Login</button>
-                        </form>
-                    </div>
-                </div>
+                {
+                    user ? "" : (
+                        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                            <div className="card-body">
+                                <form onSubmit={handleLogin} className="fieldset">
+                                    <label className="fieldset-label">Email</label>
+                                    <input type="email" className="input" name='email' placeholder="Email" />
+                                    <label className="fieldset-label">Password</label>
+                                    <input type="password" className="input" name='password' placeholder="Password" />
+                                    <div><a className="link link-hover">Forgot password?</a></div>
+                                    <button type='submit' className="btn btn-neutral mt-4">Login</button>
+                                </form>
+                            </div>
+                        </div>
+                    )
+                }
             </div>
         </div>
     );
