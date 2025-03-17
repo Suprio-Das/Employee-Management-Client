7b2a6c0e2782f8f170770e382f4552dd7954c233
Suprio-Das <priyamd507@gmail.com>
2025-03-18 00:24:34 +0600

    Static profile card designed

--- src/Components/PrivateRoutes.jsx
+++ src/Components/PrivateRoutes.jsx
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { AuthContext } from '../Provider/AuthProvider';
 
-const PrivateRoutes = ({ children }) => {
+const PrivateRoutes = () => {
     const { loading, user } = useContext(AuthContext);
     if (loading) {
         return <p>Loading</p>
     }
     if (user) {
-        return children;
+        return <Outlet></Outlet>;
     }
     return (
         <Navigate to='/' />
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Profile = () => {
     return (
-        <div>
-            <h1>This is profile.</h1>
+        <div className='w-[70%] min-h-screen flex flex-col justify-center items-center mx-auto'>
+            <div className="w-sm mx-auto bg-white rounded-2xl shadow-md overflow-hidden p-6 text-center border border-orange-500">
+                <img
+                    className="w-32 h-32 mx-auto rounded-full border-4 border-gray-200"
+                    src="https://i.ibb.co.com/BmXkp7h/my-profile-linkedin.png"
+                    alt="Profile"
+                />
+                <h2 className="text-xl font-semibold mt-4">Suprio Das</h2>
+                <p className="text-gray-600">Junior MERN Stack Developer</p>
+                <p className="text-gray-500 mt-2">📞 +880 1234-567890</p>
+                <button className="mt-4 px-4 py-2 bg-orange-500 text-white rounded-lg cursor-pointer hover:bg-orange-600 transition">
+                    Update Profile
+                </button>
+            </div>
         </div>
     );
 };
