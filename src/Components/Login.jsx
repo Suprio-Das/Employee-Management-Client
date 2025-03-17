65e039a042fd4a8875dd2fd1429d83ef42ead898
Suprio-Das <priyamd507@gmail.com>
2025-03-17 22:19:05 +0600

    User signin method implemented using firebase

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../Provider/AuthProvider';
 
 const Login = () => {
+    const { loading, signIn } = useContext(AuthContext);
+    const handleLogin = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log({ email, password });
+        signIn(email, password)
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(error => {
+                console.log(error.message);
+            })
+    }
     return (
         <div>
             <div className="bg-base-200 min-h-[calc(100vh-65px)] flex flex-col justify-center items-center">
@@ -9,14 +25,14 @@ const Login = () => {
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <div className="card-body">
-                        <fieldset className="fieldset">
+                        <form onSubmit={handleLogin} className="fieldset">
                             <label className="fieldset-label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
+                            <input type="email" className="input" name='email' placeholder="Email" />
                             <label className="fieldset-label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
+                            <input type="password" className="input" name='password' placeholder="Password" />
                             <div><a className="link link-hover">Forgot password?</a></div>
-                            <button className="btn btn-neutral mt-4">Login</button>
-                        </fieldset>
+                            <button type='submit' className="btn btn-neutral mt-4">Login</button>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,11 +1,20 @@
-import React, { createContext } from 'react';
-
+import React, { createContext, useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../Provider/firebase.init';
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-    const test = 10;
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
     const authInfo = {
-        test
+        loading,
+        signIn
     }
     return (
         <AuthContext.Provider value={authInfo}>
