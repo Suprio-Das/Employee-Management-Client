05b68fd43f8da0be6f62a69908bab105d3010400
Suprio-Das <priyamd507@gmail.com>
2025-03-18 10:39:02 +0600

    Add Employee form modified and responsive

--- src/Components/AddEmployees.jsx
+++ src/Components/AddEmployees.jsx
@@ -4,28 +4,39 @@ const AddEmployees = () => {
     return (
         <div>
             <h1 className='text-3xl font-extrabold text-center my-3'>Add Employees</h1>
-            <div className='w-[70%] mx-auto'>
+            <div className='lg:w-[70%] mx-auto my-8 bg-base-300 lg:p-11 p-2'>
                 <form>
-                    <div className='flex gap-3'>
+                    <div className='lg:flex gap-3'>
                         <fieldset className="fieldset w-full">
                             <legend className="fieldset-legend">Name</legend>
-                            <input type="text" className="input w-full" placeholder="Enter Employee Name" />
+                            <input type="text" className="input w-full" name='name' placeholder="Enter Employee Name" />
                         </fieldset>
                         <fieldset className="fieldset w-full">
                             <legend className="fieldset-legend">Email</legend>
-                            <input type="text" className="input w-full" placeholder="Enter Employee Email" />
+                            <input type="text" className="input w-full" name='email' placeholder="Enter Employee Email" />
                         </fieldset>
                     </div>
-                    <div className='flex gap-3'>
+                    <div className='lg:flex gap-3'>
                         <fieldset className="fieldset w-full">
-                            <legend className="fieldset-legend">Name</legend>
-                            <input type="text" className="input w-full" placeholder="Enter Employee Name" />
+                            <legend className="fieldset-legend">Phone</legend>
+                            <input type="text" className="input w-full" name='phone' placeholder="Enter Employee Phone" />
                         </fieldset>
                         <fieldset className="fieldset w-full">
-                            <legend className="fieldset-legend">Email</legend>
-                            <input type="text" className="input w-full" placeholder="Enter Employee Email" />
+                            <legend className="fieldset-legend">Designation</legend>
+                            <input type="text" className="input w-full" name='designation' placeholder="Enter Employee Designation" />
                         </fieldset>
                     </div>
+                    <div className='lg:flex gap-3'>
+                        <fieldset className="fieldset w-full">
+                            <legend className="fieldset-legend">Department</legend>
+                            <input type="text" className="input w-full" name='department' placeholder="Enter Employee Department" />
+                        </fieldset>
+                        <fieldset className="fieldset w-full">
+                            <legend className="fieldset-legend">Profile URL</legend>
+                            <input type="text" className="input w-full" name='photo' placeholder="Enter Employee Prfile URL" />
+                        </fieldset>
+                    </div>
+                    <input type="submit" className='btn btn-neutral w-full mt-5' value="Add Employee" />
                 </form>
             </div>
         </div>
