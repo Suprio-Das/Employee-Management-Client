059abfae0efe6e8646e2973ab021e8b8df80204d
Suprio-Das <priyamd507@gmail.com>
2025-03-18 10:26:19 +0600

    Add Employee Two Input Field Created

--- src/Components/AddEmployees.jsx
+++ src/Components/AddEmployees.jsx
@@ -3,7 +3,31 @@ import React from 'react';
 const AddEmployees = () => {
     return (
         <div>
-            <h1>This is Add Employees.</h1>
+            <h1 className='text-3xl font-extrabold text-center my-3'>Add Employees</h1>
+            <div className='w-[70%] mx-auto'>
+                <form>
+                    <div className='flex gap-3'>
+                        <fieldset className="fieldset w-full">
+                            <legend className="fieldset-legend">Name</legend>
+                            <input type="text" className="input w-full" placeholder="Enter Employee Name" />
+                        </fieldset>
+                        <fieldset className="fieldset w-full">
+                            <legend className="fieldset-legend">Email</legend>
+                            <input type="text" className="input w-full" placeholder="Enter Employee Email" />
+                        </fieldset>
+                    </div>
+                    <div className='flex gap-3'>
+                        <fieldset className="fieldset w-full">
+                            <legend className="fieldset-legend">Name</legend>
+                            <input type="text" className="input w-full" placeholder="Enter Employee Name" />
+                        </fieldset>
+                        <fieldset className="fieldset w-full">
+                            <legend className="fieldset-legend">Email</legend>
+                            <input type="text" className="input w-full" placeholder="Enter Employee Email" />
+                        </fieldset>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
